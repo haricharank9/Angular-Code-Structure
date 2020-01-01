a3ae3a089f67d0d68da71f71ab344fe329c767a8
HARI CHARAN K <hcharantemp@nabp.pharmacy>
2020-01-01 14:04:01 -0600

    ### Description### : --------------------------------------- Audited NPM package dependencies ### Components-Changed### : ------------------------------------------------------------------------------
    ### Jira### :
    ---------------------------------------

--- angular-code-structure/src/app/example-module/example-component/example-component.component.ts
+++ angular-code-structure/src/app/example-module/example-component/example-component.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-example-component',
   templateUrl: './example-component.component.html',
-  styleUrls: ['./example-component.component.css']
+  styleUrls: ['./example-component.component.scss']
 })
 export class ExampleComponentComponent implements OnInit {
 
